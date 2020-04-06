6d19a42b8745bd809ff4355eccefeca9884da2e9
Simon <simonbruce0@gmail.com>
2020-04-05 23:57:34 -0400

    Updated testAll, added readme

--- Asst1/testAll.sh
+++ Asst1/testAll.sh
@@ -1,42 +1,114 @@
 #!/bin/bash
-TESTDIR=./testcases
-FILES=$TESTDIR/test*.txt
+TESTDIR_ORIG=./testcases
+TESTDIR=./testcases_tmp
 EXE=./fileCompressor
 
 # Cleanup
-rm -rf $TESTDIR/*.hcz
-rm -rf $TESTDIR/*.hcz.txt
-rm -rf HuffmanCodebook
+cleanUp()
+{
+  if [ -f ./HuffmanCodebook ]
+  then
+    rm -rf ./HuffmanCodebook
+  fi
+  find $TESTDIR_ORIG -type f -name "*.hcz*" -exec rm "{}" \;
+  find $TESTDIR_ORIG -type f -name ".gitkeep" -exec rm "{}" \;
+  if [ -d $TESTDIR ]
+  then
+    find $TESTDIR -type f -name "*.hcz*" -exec rm "{}" \;
+  fi
+}
+
+#-----------------------------------
+# Main
+#-----------------------------------
+cleanUp
+rm -rf $TESTDIR
 rm -rf ./testoutput
-
-
+# Make a copy of the original test directory so we can compare files later.
+cp -R $TESTDIR_ORIG $TESTDIR
 mkdir ./testoutput
 
-# build, compress, decompress and compare.
-for f in $FILES
-do
-  echo "Processing test file $f"
-  # take action on each file. $f store current file name
-  bf=$(basename $f)
-  #fh=${f%.txt}.hcz // Replaces .txt with .hcz - Not what the program does...
-  fh=$f.hcz
+# 1. Run test files in testcases dirs recursively one by one.
+echo -e "Processing test files in single mode.  Dir: $TESTDIR\n"
 
+for f in $(find $TESTDIR -type f);
+do
+  echo "Processing single test file: $f"
+
+  bf=$(basename $f) # Base filename
+  origf=$(echo $f| sed "s+$TESTDIR+$TESTDIR_ORIG+") # Path and filename in original testcases dir 
+  fh=$f.hcz # filename with .hcz
+
+  # 1. Build codebook
   #echo "$EXE -b $f > ./testoutput/output_b_$bf 2>&1"
   $EXE -b $f > ./testoutput/output_b_$bf 2>&1
 
+  # 2. Compress file
   #echo "$EXE -c $f ./HuffmanCodebook > ./testoutput/output_c_$bf 2>&1"
   $EXE -c $f ./HuffmanCodebook > ./testoutput/output_c_$bf 2>&1
 
+  # Delete file so it can be recreated by decompressing.
+  rm $f
+
+  # 3. Decompress file
   #echo "$EXE -d $fh ./HuffmanCodebook > ./testoutput/output_d_$bf 2>&1"
   $EXE -d $fh ./HuffmanCodebook > ./testoutput/output_d_$bf 2>&1
 
-  echo "Comparing: diff $f $fh.txt"
-  diff $f $fh.txt
+  # 4. Compare file with original. Check that new file exists.  Diff+wc will return 0 if file is missing.
+  a=($(diff $f $origf | wc -l))
+  lines=${a[0]}
+  if [ ! -f $f ] || [ $lines -gt 0 ]
+  then
+    echo "Comparing: diff $f $origf: different!  ============= FAILED =============="
+  else
+    echo "Comparing: diff $f $origf: SUCCESS"
+  fi
 
-  #diff output_i_$bf expected_i_$bf
-
-  echo -e "\n"
+  echo -e
 done
+
+echo -e "\n"
+cleanUp
+
+# Do recursive test
+echo -e "Processing test files in recursive mode.  Dir: $TESTDIR\n"
+
+bdir=$(basename $TESTDIR)
+
+# 1. Build codebook
+#echo "$EXE -b $TESTDIR > ./testoutput/output_b_$TESTDIR 2>&1"
+$EXE -R -b $TESTDIR > ./testoutput/output_b_$bdir 2>&1
+
+# 2. Compress all files in dir
+#echo "$EXE -c $TESTDIR ./HuffmanCodebook > ./testoutput/output_c_$TESTDIR 2>&1"
+$EXE -R -c $TESTDIR ./HuffmanCodebook > ./testoutput/output_c_$bdir 2>&1
+
+# Delete files so they can be recreated by decompressing.
+find $TESTDIR -type f -not -name "*.hcz*" -exec rm "{}" \;
+
+# 3. Decompress all files in dir
+#echo "$EXE -d $TESTDIR ./HuffmanCodebook > ./testoutput/output_d_$TESTDIR 2>&1"
+$EXE -R -d $TESTDIR ./HuffmanCodebook > ./testoutput/output_d_$bdir 2>&1
+
+# 4. Loop over all text files recursively and compare with the compressed file
+for f in $(find $TESTDIR -type f -not -name "*.hcz*");
+do
+  bf=$(basename $f) # Base filename
+  origf=$(echo $f| sed "s+$TESTDIR+$TESTDIR_ORIG+") # Path and filename in original testcases dir 
+
+  # Compare file with original.  Check that new file exists.  Diff+wc will return 0 if file is missing.
+  a=($(diff $f $origf | wc -l))
+  lines=${a[0]}
+  if [ ! -f $f ] || [ $lines -gt 0 ]
+  then
+    echo "Comparing: diff $f $origf: different!  ============= FAILED =============="
+  else
+    echo "Comparing: diff $f $origf SUCCESS"
+  fi
+
+  echo -e
+done
+
 
 # echo "Testing bad arguments: No args"
 # ../a.out > output_noargs.txt 2>&1
--- Asst1/testClean.sh
+++ Asst1/testClean.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-TESTDIR=./testcases
-FILES=$TESTDIR/test*.txt
-EXE=./fileCompressor
+TESTDIR_ORIG=./testcases
+TESTDIR=./testcases_tmp
 
-# Cleanup
-rm -rf $TESTDIR/*.hcz
-rm -rf $TESTDIR/*.hcz.txt
 rm -rf HuffmanCodebook
-rm -rf ./fileCompressor
+find $TESTDIR_ORIG -type f -name "*.hcz*" -exec rm "{}" \;
+find $TESTDIR_ORIG -type f -name ".gitkeep" -exec rm "{}" \;
+find $TESTDIR -type f -name "*.hcz*" -exec rm "{}" \;
 
+if [ -f ./HuffmanCodebook ]
+then
+  rm -rf ./HuffmanCodebook
+fi
+find $TESTDIR_ORIG -type f -name "*.hcz*" -exec rm "{}" \;
+find $TESTDIR_ORIG -type f -name ".gitkeep" -exec rm "{}" \;
+if [ -d $TESTDIR ]
+then
+  find $TESTDIR -type f -name "*.hcz*" -exec rm "{}" \;
+fi
+
+if [ -d $TESTDIR ]
+then
+  rm -rf $TESTDIR
+fi  
+if [ -d ./testoutput ]
+then
+  rm -rf ./testoutput
+fi
